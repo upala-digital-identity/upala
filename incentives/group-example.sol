9141ed728a6f9bc7e32e40c07e761274a699c285
Petr Porobov <porobov.p@gmail.com>
2019-12-11 11:46:22 +0400

    Groups as a standard

--- incentives/group-example.sol
+++ incentives/group-example.sol
@@ -1,35 +1,63 @@
-contract GroupTypesRegister {
-    function start(address) external returns (uint) {}
+pragma solidity ^0.5.0;
+
+interface IUpalaGroup {
+
+// function getPoolSize()
+// function getMaxBotReward()
+// function getMemberScore(address member) 
+
+// function attack(address[] _path)
+// function rageQuit()
+
 }
 
-contract Group {
-	// Honey pot
+contract UpalaGroup is IUpalaGroup {
+
+    // Honey pot
 	uint pool;  // Eth pool of the group
 	uint maxBotReward;  // 
 
-	// Group and members types
-	bool lowestLevel = false;  // false - members are groups, true - members are users
-	byte8 groupType = "Hash-Of-Registered-GroupType";
-	byte8[] allowedMemberTypes;  // list of allowed types hashes (not applicable for lowest level groups)
-
-	// members and scores d
-	// address[] members;
+	// members and scores
 	mapping(address => uint8) membersScores;  // 0-100% Personhood; 0 - not a member?
+
+	function getMemberScore(address member) returns (uint8) {
+		// ... descend the path and calc score!!!
+		return (membersScores[member]);
+
+	}
+
+	// Bots
+	//function attack(uint personhoodScore, bytes8 proofOfScore) {
+	// ... descend the path and do harm!!!
+	function attack(address[] _path) {
+		calculateScore(msg.sender, _path);
+		reward = maxBotReward * personhoodScore;
+		msg.sender.send(reward);
+		// refund by subgroups - pull rewards. 
+	}
+
+	// RageQuit???
+	function rageQuit() onlyMember {
+		rageQuitTrigger();  // exit conditions defined for the group
+	}
+}
+
+
+
+contract Group is UpalaGroup, Ownable {
 	
-	// 
-	address admin;  // either a trusted user or a voting contract
+	// Group and members types
+	byte8 groupType = "Hash-Of-Registered-GroupType"; // 0 - members are users
+	byte8[] allowedMemberTypes;  // list of allowed types hashes (not applicable for lowest level groups)
+	
+    // owner is either a trusted user or a voting contract
 	// sets entering condition including income pool (income) management, types of groups to be invited
 
-
-	modifier onlyAdmin() {
-		require (msg.sender == admin);  // todo
-		_; 
-	}
-
-	function setNewAdmin(address newAdmin) external onlyAdmin {
-		admin = newAdmin;
-	}
 	
+	// Apps
+	function calculateUserScore(address candidate, address[] _path) {  //payable optionally
+
+	}
 
 	function updateMemberScore(address member, uint8 newScore) onlyAdmin {
 		// todo check member type
@@ -42,25 +70,4 @@ contract Group {
 		pool+= msg.value;
 	}
 
-	
-	// Bots
-	//function attack(uint personhoodScore, bytes8 proofOfScore) {
-	function attack(address[] _path) {
-		// ... descend the path and do harm!!!
-		calculateScore(msg.sender, _path);
-		reward = maxBotReward * personhoodScore;
-		msg.sender.send(reward);
-	}
-
-	// Apps
-	function checkScore(address candidate, address[] _path) returns (uint8) {
-		// ... descend the path and calc score!!!
-		return (calculateScore(candidate, _path));
-
-	}
-
-	// RageQuit???
-	function rageQuit() onlyMember {
-		rageQuitTrigger();  // exit conditions defined for the group
-	}
 }
