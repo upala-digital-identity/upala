44f50ca8c103ece9a2720e7c74985fb038a96e6c
Petr Porobov <porobov.p@gmail.com>
2020-04-15 14:59:57 +0400

    Exchange ETH for DAI

--- contracts/mockups/fake-dai-mock.sol
+++ contracts/mockups/fake-dai-mock.sol
@@ -3,19 +3,14 @@ pragma solidity ^0.6.0;
 import "../libraries/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract FakeDai is ERC20 {
+
     // s
     function freeDaiToTheWorld(address anyAccount, uint256 anyAmount) external {
         _mint(anyAccount, anyAmount);
     }
 
-
-    // function _mint(address account, uint256 amount) internal virtual {
-    //     require(account != address(0), "ERC20: mint to the zero address");
-
-    //     _beforeTokenTransfer(address(0), account, amount);
-
-    //     _totalSupply = _totalSupply.add(amount);
-    //     _balances[account] = _balances[account].add(amount);
-    //     emit Transfer(address(0), account, amount);
-    // }
+    // send ETH, receive FakeDAI
+    function exchangeETHtoFDAI() external payable {
+        _mint(msg.sender, msg.value);
+    }
 }
