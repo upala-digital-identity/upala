f0e62457750f76eed38287d9236e6c60034b06bf
Petr Porobov <porobov.p@gmail.com>
2020-05-15 12:23:19 +0400

    removed front-end dir and truffle

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const Upala = artifacts.require("Upala");
-const FakeDai = artifacts.require("FakeDai");
-
-module.exports = function(deployer) {
-  deployer.deploy(Upala);
-  deployer.deploy(FakeDai);
-};
