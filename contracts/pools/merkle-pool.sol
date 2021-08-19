1002092e8fb39fe3a7f729eb6a326e36825f3cb1
Petr Porobov <porobov.p@gmail.com>
2021-08-19 13:08:06 +0400

    Common BudledScoresPool for Merkle pool and Signed scores pool

--- contracts/pools/merkle-pool.sol
+++ contracts/pools/merkle-pool.sol
@@ -1,12 +1,8 @@
 pragma solidity ^0.6.0;
 
-
-import "../libraries/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "../libraries/openzeppelin-contracts/contracts/math/SafeMath.sol";
-import "../libraries/openzeppelin-contracts/contracts/access/Ownable.sol";
+import 'contracts/pools/bundledScoresPool.sol';
 import "./i-pool-factory.sol";
 import "./i-pool.sol";
-import "../protocol/upala.sol";
 import "hardhat/console.sol";
 /*
 
@@ -40,135 +36,44 @@ contract MerklePoolFactory {
 
 // The most important obligation of a group is to pay bot rewards.
 // MerkleTreePool
-contract MerklePool is Ownable {
-    using SafeMath for uint256;
+contract MerklePool is BundledScoresPool {
 
-    Upala public upala;
-    IERC20 public approvedToken; // approved token contract reference
+    constructor(
+        address upalaAddress,
+        address approvedTokenAddress,
+        address poolManager) 
+    BundledScoresPool(
+        upalaAddress, 
+        approvedTokenAddress, 
+        poolManager) 
+    public {}
 
-    /******
-    SCORING
-    ******/
-
-    // base reward get multiplied by individual scores to get total score for each user
-    // with base reward we can tweak all users scores simultaneously
-    uint256 public baseScore;
-    // merkle roots of trees storing scores
-    mapping (bytes32 => uint256) public roots;
-
-    /************
-    ANNOUNCEMENTS
-    *************/
-
-    // Any changes that can hurt bot rights must wait for an attackWindow to expire
-    mapping(bytes32 => uint) public commitsTimestamps;
-
-    /*****
-    EVENTS
-    *****/
-
-    event Claimed(
-        uint256 _index,
-        address _identity,
-        uint256 _score
-    );
-
-    constructor(address upalaAddress, address approvedTokenAddress, address poolManager) public {
-        baseScore = 1;
-        upala = Upala(upalaAddress);
-        approvedToken = IERC20(approvedTokenAddress);
-        transferOwnership(poolManager);
-        
-    }
-
-    /*********************
-    SCORING AND BOT ATTACK
-    **********************/
-
-    // ####### Hackathon mocks begin ##########
-
-    // a mock function before real Merkle is implemented
-    function verifyHack() public returns(bool res) { 
-        return true;
-    }
-
-    // a mock function before real Merkle is implemented
-    function getRootHack(bytes32 sfsdf) public returns(uint res) {
-        return 1;
-    }
-
-    // for DApps - hackathon mock
-    function verifyUserScoreHack (address groupID, address identityID, address ownerOrDelegate, uint8 score, bytes32[] calldata proof) external returns (bool) {
-        return true;
-    }
-
-    // ####### Hackathon mocks end ##########
-
-    // 
-    function userScore(address identityID, address userAddress, uint256 index, uint8 score, bytes32[] calldata proof) external returns (uint256) {
-        // calculate score (and check validity)
-        uint256 totalScore = _userScore(identityID, userAddress, index, score, proof);
-        // todo fee to group
-        return totalScore;
-    }
-
-    // multipass 
-    function myScore(uint256 index, address identityID, uint8 score, bytes32[] calldata proof) external view returns (uint256) {
-        
-        // calculate score (and check validity)
-        uint256 totalScore = _userScore(identityID, msg.sender, index, score, proof);
-
-        return totalScore;
-    }
-
-
-
-    // Allows any identity to attack any group, run with the money and self-destruct.
-    // todo no nonReentrant?
-    function attack(address identityID, uint256 index, uint8 score, bytes32[] calldata proof)
-        external
-    {   
-        console.log("hey");
-        // calculate reward (and validity)
-        uint256 reward = _userScore(identityID, msg.sender, index, score, proof);
-        console.log(reward);
-        // explode (delete id forever)
-        upala.explode(identityID);
-
-        // payout 💸
-        _payBotReward(msg.sender, reward);
-    }
-
-    function hack_computeRoot(uint256 index, address identityID, uint8 score, bytes32[] calldata proof) external view returns (bytes32) {
-        uint256 hack_score = uint256(score);
-        bytes32 leaf = keccak256(abi.encodePacked(index, identityID, hack_score));
-        return _computeRoot(proof, leaf);
-    }
-
-    function hack_leaf(uint256 index, address identityID, uint8 score, bytes32[] calldata proof) external view returns (bytes32) {
-        uint256 hack_score = uint256(score);
-        return  keccak256(abi.encodePacked(index, identityID, hack_score));
-    }
-
-    function _userScore(address identityID, address ownerOrDelegate, uint256 index, uint8 score, bytes32[] memory proof) private view returns (uint256){
-        // check identity validity
-        console.log("_userScore");
-        require(upala.isOwnerOrDelegate(ownerOrDelegate, identityID), "Address doesn't own an ID or is exploded");
-        // TODO check that pool balance is sufficient for explosion
-        // check Merkle proof
-        uint256 hack_score = uint256(score);
-        bytes32 leaf = keccak256(abi.encodePacked(index, identityID, hack_score));
-        console.logBytes32(leaf);
+    function isInBundle(
+        address intraBundleUserID,
+        uint8 score,
+        bytes32 bundleId,
+        bytes memory indexAndProof
+    ) internal view override returns (bool) {
+        uint256 index = hack_extractIndex(indexAndProof); 
+        bytes32 leaf = keccak256(abi.encodePacked(index, intraBundleUserID, score));
+        bytes32[] memory proof = hack_extractProof(indexAndProof);
         bytes32 computedRoot = _computeRoot(proof, leaf);
-        console.logBytes32(computedRoot);
-        require (roots[computedRoot] > 0, 'MerkleDistributor: Invalid proof.');
-        
-        uint256 totalScore = baseScore * score;
-        
-        return totalScore;
+        return(scoreBundleTimestamp[computedRoot] > 0);
     }
 
-    function _computeRoot(bytes32[] memory proof, bytes32 leaf) internal pure returns (bytes32) {
+    // todo
+    function hack_extractIndex(bytes memory indexAndProof) private pure returns (uint256) {
+        return (1);
+    }
+
+    // todo
+    function hack_extractProof(bytes memory indexAndProof) private pure returns (bytes32[] memory) {
+        bytes32[] memory extracted;
+        extracted[0] = bytes32("candidate1");
+        return (extracted);
+    }
+
+    function _computeRoot(bytes32[] memory proof, bytes32 leaf) private pure returns (bytes32) {
         bytes32 computedHash = leaf;
         
         for (uint256 i = 0; i < proof.length; i++) {
@@ -186,119 +91,14 @@ contract MerklePool is Ownable {
         return computedHash;
     }
 
-    /***********
-    MANAGE GROUP
-    ************/
-
-    /*Announcements*/
-    // Announcements prevents front-running bot-exposions. Groups must announce
-    // in advance any changes that may hurt bots rights
-
-    // hash = keccak256(action-type, [parameters], secret)
-    function commitHash(bytes32 hash) external onlyOwner returns(uint256 timestamp)  {
-        uint256 timestamp = now;
-        commitsTimestamps[hash] = timestamp;
-        return timestamp;
-    }
- 
-    function checkHash(bytes32 hash) internal view returns(bool){
-        
-        require (commitsTimestamps[hash] != 0, "No such commitment hash");
-        require (commitsTimestamps[hash] + upala.attackWindow() <= now, "Attack window is not closed yet");
-        require (commitsTimestamps[hash] + upala.attackWindow() + upala.executionWindow() >= now, "Execution window is already closed");
-        // todo is it possible to create lock for active commits when changing windows?
-        return true;
+    function hack_computeRoot(uint256 index, address identityID, uint8 score, bytes32[] calldata proof) external view returns (bytes32) {
+        uint256 hack_score = uint256(score);
+        bytes32 leaf = keccak256(abi.encodePacked(index, identityID, hack_score));
+        return _computeRoot(proof, leaf);
     }
 
-    /*Changes that may hurt bots rights*/
-
-    // Sets the maximum possible bot reward for the group.
-    function setBaseScore(uint botReward, bytes32 secret) external onlyOwner {
-        bytes32 hash = keccak256(abi.encodePacked("setBaseScore", botReward, secret));
-        // todo not checknig hash timestamp
-        checkHash(hash);
-        baseScore = botReward;
-        delete commitsTimestamps[hash];
-        // emit Set("NewBotReward", group, botReward);
-    }
-
-    function deleteRoot(bytes32 root, bytes32 secret) external onlyOwner {
-        bytes32 hash = keccak256(abi.encodePacked("deleteRoot", root, secret));
-        checkHash(hash);
-        require(commitsTimestamps[hash] > roots[root], "Commit is submitted before root");
-        delete commitsTimestamps[hash];
-        delete roots[root];
-    }
-
-    // tries to withdraw as much as possible (bots could have attacked after an announcement) 
-    function withdrawFromPool(address recipient, uint amount, bytes32 secret) external onlyOwner returns (uint256){ // $$$
-        bytes32 hash = keccak256(abi.encodePacked("withdrawFromPool", secret));
-        checkHash(hash);
-        uint256 withdrawnAmount = _withdrawAvailable(recipient, amount);
-        delete commitsTimestamps[hash];
-        // emit Set("withdrawFromPool", withdrawed);
-        return withdrawnAmount;
-    }
-
-    /*Changes that don't hurt bots rights*/
-
-    function increaseBaseScore(uint newBotReward) external onlyOwner {
-        require (newBotReward > baseScore, "To decrease score, make a commitment first");
-        baseScore = newBotReward;
-    }
-
-    // todo onlyOwner
-    // note don't care for publishing same root? As it is Merkle, 
-    // the data inside would still be the same
-    function publishScoreBundle(bytes32 newRoot) external  {
-        console.log("publishRoot");
-        
-        roots[newRoot] = now;
-    }
-
-    // function upgradePool(address poolFactory, bytes32 secret) external returns (address, uint256) {
-    // }
-
-
-    /****
-    FUNDS
-    *****/
-
-    // Upala checks funds to make sure the pool has enough funds to fund a bot attack
-    function hasEnoughFunds(uint256 ammount) private view returns(bool) {
-        return (approvedToken.balanceOf(address(this)) >= ammount);
-    }
-
-    // bots are getting paid instantly
-    function _payBotReward(address bot, uint amount) private returns(bool)  {
-        require(_withdraw(bot, amount), "token transfer to bot failed");
-        return true;
-    }
-
-    function _withdrawAvailable(address receiver, uint256 amount) private returns (uint256 whitdrawnAmount) {
-        uint256 balance = approvedToken.balanceOf(address(this));
-        if (balance >= amount) {
-            _withdraw(receiver, amount);
-            return amount;
-        } else {
-            _withdraw(receiver, balance);
-            return balance;
-        }
-    }
-
-    function _withdraw(address recipient, uint amount) private returns (bool) {
-        return approvedToken.transfer(recipient, amount);
-    }
-
-    /**************
-    GETTER FUNCTIONS
-    ***************/
-
-    function getRootTimestamp(bytes32 root) external returns (uint256) {
-        return roots[root];
-    }
-
-    function groupBaseScore() external view returns (uint) {
-        return baseScore;
+    function hack_leaf(uint256 index, address identityID, uint8 score, bytes32[] calldata proof) external view returns (bytes32) {
+        uint256 hack_score = uint256(score);
+        return  keccak256(abi.encodePacked(index, identityID, hack_score));
     }
 }
--- contracts/pools/signed-scores-pool.sol
+++ contracts/pools/signed-scores-pool.sol
@@ -1,19 +1,10 @@
 pragma solidity ^0.6.0;
 
-import '../libraries/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
-import '../libraries/openzeppelin-contracts/contracts/math/SafeMath.sol';
-import '../libraries/openzeppelin-contracts/contracts/access/Ownable.sol';
+import 'contracts/pools/bundledScoresPool.sol';
 import '../libraries/openzeppelin-contracts/contracts/cryptography/ECDSA.sol';
 import './i-pool-factory.sol';
-import './i-pool.sol';
 import '../protocol/upala.sol';
 import 'hardhat/console.sol';
-
-/*
-
-Every group to manages its poool in it's own way.
-Or even to share one pool among several groups.
-*/
 
 // production todo create IPool
 contract SignedScoresPoolFactory {
@@ -38,312 +29,18 @@ contract SignedScoresPoolFactory {
 }
 
 // The most important obligation of a group is to pay bot rewards.
-contract SignedScoresPool is Ownable {
-    using SafeMath for uint256;
+contract SignedScoresPool is BundledScoresPool {
     using ECDSA for bytes32;
-
-    Upala public upala;
-
-    // Dai ERC20 token for the mainnet (fakeDAI for testing)
-    IERC20 public approvedToken; // approved token contract reference
-
-    // Metadata. e.g. { title, url, joinLink, dbUrl }
-    string public metaData; // json object for future use
-
-    /******
-    SCORING
-    ******/
-
-    // Base reward gets multiplied by individual scores to get 
-    // total score for each user.
-    // With base reward we can tweak all users scores simultaneously
-    uint256 public baseScore;
-
-    // When verifying their score or exploding, users prove they are in a 
-    // score bundle. 
-    mapping(bytes32 => uint256) public scoreBundleIds;
-
-    /************
-    ANNOUNCEMENTS
-    *************/
-
-    // Any changes that can hurt bot rights must wait for an attackWindow
-    mapping(bytes32 => uint256) public commitsTimestamps;
-    
-    /*****
-    EVENTS
-    *****/
-
-    event MetaDataUpdate(string metadata);
-    event NewScoreBundleId(bytes32 newScoreBundleId, uint256 timestamp);
-    event ScoreBundleIdDeleted(bytes32 newScoreBundleId);
-    event NewBaseScore(uint256 newBaseScore);
 
     constructor(
         address upalaAddress,
         address approvedTokenAddress,
-        address poolManager
-    ) public {
-        upala = Upala(upalaAddress);
-        approvedToken = IERC20(approvedTokenAddress);
-        transferOwnership(poolManager);
-    }
-
-    
-    /***********
-    MANAGE GROUP
-    ************/
-
-    /* Announcements */
-    // Announcements prevents front-running bot-exposions. Groups must announce
-    // in advance any changes that may hurt bots rights
-    // hash = keccak256(action-type, [parameters], secret) - see below
-
-    function commitHash(bytes32 hash) 
-        external 
-        onlyOwner 
-        returns (uint256 timestamp) 
-    {
-        uint256 timestamp = now;
-        commitsTimestamps[hash] = timestamp;
-        return timestamp;
-    }
-
-    modifier hasValidCommit(bytes32 hash) {
-        require(commitsTimestamps[hash] != 0, 
-            'No such commitment hash');
-        require(commitsTimestamps[hash] + upala.attackWindow() <= now, 
-            'Attack window is not closed yet');
-        require(
-            commitsTimestamps[hash] + upala.attackWindow() + upala.executionWindow() >= now,
-            'Execution window is already closed'
-        );
-        _;
-        delete commitsTimestamps[hash];
-    }
-
-    /*Changes that may hurt bots rights (require an announcement)*/
-
-    // todo should this apply to all commits?
-    // require(scoreBundleIds[scoreBundleId] > now + attackWindow, 
-    // 'Commit is submitted before scoreBundleId');
-    
-    // Sets the the base score for the group.
-    function setBaseScore(uint256 newBaseScore, bytes32 secret)
-        external
-        onlyOwner
-        hasValidCommit(keccak256(abi.encodePacked(
-            'setBaseScore', newBaseScore, secret)))
-    {
-        baseScore = newBaseScore;
-        NewBaseScore(newBaseScore);
-    }
-
-    function deleteScoreBundleId(bytes32 scoreBundleId, bytes32 secret) 
-        external 
-        onlyOwner
-        hasValidCommit(keccak256(abi.encodePacked(
-            'deleteScoreBundleId', scoreBundleId, secret)))
-    {
-        delete scoreBundleIds[scoreBundleId];
-        ScoreBundleIdDeleted(scoreBundleId);
-    }
-
-    function withdrawFromPool(address recipient, uint256 amount, bytes32 secret) 
-        external 
-        onlyOwner
-        hasValidCommit(keccak256(abi.encodePacked(
-            'withdrawFromPool', secret)))
-        returns (uint256) 
-    {
-        // event is triggered by DAI contract
-        return _withdrawAvailable(recipient, amount);
-    }
-
-    /*Changes that don't hurt bots rights*/
-
-    function increaseBaseScore(uint256 newBaseScore) external onlyOwner {
-        require(newBaseScore > baseScore, 
-            'To decrease score, make a commitment first');
-        baseScore = newBaseScore;
-        NewBaseScore(newBaseScore);
-    }
-
-    // Hashes are used for compatibility with Merkle pools.
-    function publishScoreBundleId(bytes32 newBundleId) external onlyOwner returns (uint256) {
-        require(scoreBundleIds[newBundleId] == 0, 
-            'Score bundle id already exists');
-        scoreBundleIds[newBundleId] = now;
-        NewScoreBundleId(newBundleId, now);
-        return now;
-    }
-
-    function updateMetadata(string calldata newMetadata) external onlyOwner {
-        metaData = newMetadata;
-        MetaDataUpdate(newMetadata);
-    }
-
-    /****
-    FUNDS
-    *****/
-
-    // Upala checks funds to make sure the pool has enough funds for attack
-    function _balanceIsAbove(uint256 ammount) private view returns (bool) {
-        return (approvedToken.balanceOf(address(this)) >= ammount);
-    }
-
-    // bots are getting paid instantly
-    function _payBotReward(address bot, uint256 amount) private {
-        uint256 fee = amount.mul(upala.explosionFeePercent()).div(100);
-        require(_withdraw(bot, amount.sub(fee)), 
-            'Token transfer to bot failed');
-        // UIP-6. Sustainability + mitigating withdrawals by explosion
-        require(_withdraw(upala.treasury(), fee), 
-            'Explosion fee transfer failed');
-    }
-
-    // tries to withdraw as much as possible 
-    // (bots can attack after an announcement)
-    function _withdrawAvailable(address receiver, uint256 amount) 
-        private 
-        returns (uint256 whitdrawnAmount) 
-    {
-        uint256 balance = approvedToken.balanceOf(address(this));
-        if (balance >= amount) {
-            _withdraw(receiver, amount);
-            return amount;
-        } else {
-            _withdraw(receiver, balance);
-            return balance;
-        }
-    }
-
-    function _withdraw(address recipient, uint256 amount) private returns (bool) {
-        return approvedToken.transfer(recipient, amount);
-    }
-
-    /***********
-    DAPP PAYWALL
-    ************/
-
-    // modifier verificationFeeApplied() {
-    //     require(paywall.charge(msg.sender, score));
-    //     _;
-    // }
-
-    // modifier registrationFeeApplied() {
-    //     require(paywall.chargeRegistration(msg.sender));
-    //     _;
-    // }
-
-    // // Paywalls charge only DApps. They have their own balances.
-    // // No need to prevent bot explosion front-run
-    // function appendPaywall(address newPaywall) {
-    //     require(upala.isApprovedPaywall(newPaywall) == true,
-    //         "Paywall not approved");
-    //     require(Paywall(newPaywall).append() == true,
-    //         "Paywall denies pool");
-    //     paywall = Paywall(newPaywall);
-    // }
-
-    /**************
-    GETTER FUNCTIONS
-    ***************/
-
-    function getScoreBundleIdTimestamp(bytes32 scoreBundleId) external returns (uint256) {
-        return scoreBundleIds[scoreBundleId];
-    }
-
-    function groupBaseScore() external view returns (uint256) {
-        return baseScore;
-    }
-
-    /*********************
-    SCORING AND BOT ATTACK
-    **********************/
-    // Pool-specific 
-
-    // tests only for now
-    function myScore(
-        address upalaID,
-        address scoreAssignedTo,
-        uint8 score,
-        bytes32 bundleId,
-        bytes calldata proof
-    ) external view returns (uint256) {
-        return _userScore(msg.sender, upalaID, scoreAssignedTo, score, bundleId, proof);
-    }
-
-    // dapps
-    function userScore(
-        address userAddress,
-        address upalaID,
-        address scoreAssignedTo,
-        uint8 score,
-        bytes32 bundleId,
-        bytes calldata proof
-    )
-        external
-        view
-        /// production todo paywall modifier goes here
-        returns (uint256)
-    {
-        return _userScore(userAddress, upalaID, scoreAssignedTo, score, bundleId, proof);
-    }
-
-    // Allows any identity to attack the group (pool), 
-    // run with the money and self-destruct.
-    function attack(
-        address upalaID,
-        address scoreAssignedTo,
-        uint8 score,
-        bytes32 bundleId,
-        bytes calldata proof
-    ) external {
-        console.log('hey');
-
-        // calculate reward (and validity)
-        uint256 reward = _userScore(msg.sender, upalaID, scoreAssignedTo, score, bundleId, proof);
-
-        // explode (delete id forever)
-        upala.explode(upalaID);
-
-        // payout 💸
-        _payBotReward(msg.sender, reward);
-    }
-
-    function _userScore(
-        address ownerOrDelegate,    // user address that shoots verification call
-        address upalaID,            // user Upala ID
-        address scoreAssignedTo,    // the address used in bundle
-        uint8 score,                // assigned score
-        bytes32 bundleId,           // bundle hash (root if using Merkle pool)
-        bytes memory proof    // a proof that verifies user score is in bundle
-    ) private view returns (uint256) {
-
-        require(scoreBundleIds[bundleId] > 0, 
-            "Provided score bundle does not exist or deleted");
-
-        require(upala.isOwnerOrDelegate(ownerOrDelegate, upalaID),
-            "Not an owner or delegate. Or Upala ID is exploded");
-
-        // a way to validate by address (UIP-22)
-        // can still validate from any delegate address
-        if (scoreAssignedTo != upalaID && scoreAssignedTo != ownerOrDelegate) {
-            require(upala.isOwnerOrDelegate(scoreAssignedTo, upalaID),
-                "Score bearing address is not associated with Upala ID");
-        }
-
-        uint256 totalScore = baseScore.mul(score);
-        require(_balanceIsAbove(totalScore),
-            "Pool balance is lower than the total score");
-
-
-        require(isInBundle(scoreAssignedTo, score, bundleId, proof) == true,
-            "Can't validate that scoreAssignedTo-score pair is in the bundle");
-
-        return totalScore;
-    }
+        address poolManager) 
+    BundledScoresPool(
+        upalaAddress, 
+        approvedTokenAddress, 
+        poolManager) 
+    public {}
 
     // Pool-specific way to validate that userID is in bundle
     // SignedScoresPool requires every score to be signed by pool manager
@@ -356,7 +53,7 @@ contract SignedScoresPool is Ownable {
         uint8 score,
         bytes32 bundleId,
         bytes memory signature
-    ) private view returns (bool) {
+    ) internal view override returns (bool) {
         return(
             keccak256(abi.encodePacked(intraBundleUserID, score, bundleId))
                 .toEthSignedMessageHash()
