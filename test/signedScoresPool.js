ccfb1114cd41ccd74e0f65c6d298205eb3f9acbc
Petr Porobov <porobov.p@gmail.com>
2022-03-16 12:43:23 +0400

    Added first tests for SCORING AND BOT ATTACK

--- test/signedScoresPool.js
+++ test/signedScoresPool.js
@@ -8,12 +8,15 @@ const { expect } = require('chai')
 const { BigNumber, utils } = require('ethers')
 const { setupProtocol } = require('../src/upala-admin.js')
 const { deployPool, attachToPool, PoolManager } = require('@upala/group-manager')
+const { newIdentity } = require('@upala/unique-human')
 
 // const PoolManager = require('@upala/group-manager')
 const poolAbi = require('../artifacts/contracts/pools/signed-scores-pool.sol/SignedScoresPool.json')
 let oneETH = BigNumber.from(10).pow(18)
 //const scoreChange = oneETH.mul(42).div(100)
-
+const ZERO_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000000000000'
+const RANDOM_SCORE_42 = 42  // do not use 42 anywhere else
+const RANDOM_ADDRESS = '0x0c2788f417685706f61414e4Cb6F5f672eA79731'
 /***********
 MANAGE GROUP
 ************/
@@ -21,22 +24,20 @@ MANAGE GROUP
 describe('MANAGE GROUP', function () {
   let upalaAdmin, manager1, nobody
   let signedScoresPool
-  const ZERO_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000000000000'
-  let someRoot = ZERO_BYTES32
 
   before('setup protocol', async () => {
-    let environment = await setupProtocol({ isSavingConstants: false })
-    ;[upalaAdmin, manager1, nobody] = environment.wallets
-    signedScoresPool = await deployPool('SignedScoresPool', manager1, environment.upalaConstants)
+    let env = await setupProtocol({ isSavingConstants: false })
+    ;[upalaAdmin, manager1, nobody] = env.wallets
+    signedScoresPool = await deployPool('SignedScoresPool', manager1, env.upalaConstants)
   })
 
   it('group manager can publish new bundle', async function () {
-    await expect(signedScoresPool.connect(nobody).publishScoreBundleId(someRoot)).to.be.revertedWith(
+    await expect(signedScoresPool.connect(nobody).publishScoreBundleId(ZERO_BYTES32)).to.be.revertedWith(
       'Ownable: caller is not the owner'
     )
-    tx = await signedScoresPool.connect(manager1).publishScoreBundleId(someRoot)
+    tx = await signedScoresPool.connect(manager1).publishScoreBundleId(ZERO_BYTES32)
     let txTimestamp = (await ethers.provider.getBlock(tx.blockNumber)).timestamp
-    let bundleTimestamp = await signedScoresPool.scoreBundleTimestamp(someRoot)
+    let bundleTimestamp = await signedScoresPool.scoreBundleTimestamp(ZERO_BYTES32)
     expect(bundleTimestamp).to.eq(txTimestamp)
   })
 })
@@ -48,9 +49,9 @@ describe('MANAGE GROUP', function () {
 
   it('group manager can set base score', async function () {
     // initializing Upala manager
-    let environment = await setupProtocol({ isSavingConstants: false })
-    upala = environment.upala
-    ;[upalaAdmin, nobody] = environment.wallets
+    let env = await setupProtocol({ isSavingConstants: false })
+    upala = env.upala
+    ;[upalaAdmin, nobody] = env.wallets
 
     // inititalizing pool poolManagerWallet
     // var poolManager = new PoolManager({
@@ -84,44 +85,69 @@ describe('MANAGE GROUP', function () {
 /*********************
 SCORING AND BOT ATTACK
 **********************/
-/*
+  // strategy:
+  // use myScore function to check most of the require conditions
+  // then use userScore to check if dapps can querry scores
+  // then use attack to check funds distribution
+  // persona - use this name to describe Eth address with score
+  // nobody - not registered person
 describe('SCORING AND BOT ATTACK', function () {
-  // todo setup protocol
+  let upalaAdmin, manager1, nobody
+  let persona1id
   let upala
-  let fakeDai
-  let signedScoresPoolFactory
   let signedScoresPool
-  let wallets
-  let upalaAdmin
-  let manager1
+  let emptyScoreBundle = '0x0000000000000000000000000000000000000000000000000000000000000001'
 
-  before('register users', async () => {
-    ;[upala, fakeDai, wallets] = await setupProtocol()
-    ;[upalaAdmin, manager1] = wallets.slice(0, 2)
-    ;[signedScoresPoolFactory, signedScoresPool] = await setUpPoolFactoryAndPool(
-      upala,
-      fakeDai,
-      'SignedScoresPoolFactory',
-      upalaAdmin,
-      manager1
+  before('setup protocol, register users', async () => {
+    let env = await setupProtocol({ isSavingConstants: false })
+    ;[upalaAdmin, manager1, persona1, nobody] = env.wallets
+    signedScoresPool = await deployPool('SignedScoresPool', manager1, env.upalaConstants)
+    // register empty score bundle
+    await signedScoresPool.connect(manager1).publishScoreBundleId(emptyScoreBundle)
+    persona1id = await newIdentity(persona1.address, persona1, env.upalaConstants)
+  })
+
+  it('cannot verify scores over non-existent score bundle', async function () {
+    await expect(signedScoresPool.connect(nobody).myScore(
+      RANDOM_ADDRESS,
+      RANDOM_ADDRESS,
+      RANDOM_SCORE_42,
+      ZERO_BYTES32,
+      ZERO_BYTES32
+    )).to.be.revertedWith(
+      'Provided score bundle does not exist or deleted'
     )
   })
 
-  describe('VERIFYING SCORES', function () {
-    // strategy:
-    // use myScore function to check most of the require conditions
-    // then use userScore to check if dapps can querry scores
-    // then use attack to check funds distribution
-    // persona - use this name to describe Eth address with score
-    // nobody - not registered person
-    // setup protocol
-    // try myScore on non-existent score bundle
-    // should throw "Provided score bundle does not exist or deleted"
-    // register a score bundle
-    // try myScore on persona address withou UpalaID
-    // should throw
+  it('cannot verify scores without UpalaID', async function () {
+    // no UpalaID at all 
+    await expect(signedScoresPool.connect(nobody).myScore(
+      RANDOM_ADDRESS,
+      RANDOM_ADDRESS,
+      RANDOM_SCORE_42,
+      emptyScoreBundle,
+      ZERO_BYTES32
+    )).to.be.revertedWith(
+      'Upala: No such id, not an owner or not a delegate of the id'  // todo better 'No Upala ID'?
+    )
+    // todo existing UpalaID but not an owner 
+    // await expect(signedScoresPool.connect(nobody).myScore(
+    //   RANDOM_ADDRESS,
+    //   RANDOM_ADDRESS,
+    //   RANDOM_SCORE_42,
+    //   emptyScoreBundle,
+    //   ZERO_BYTES32
+    // )).to.be.revertedWith(
+    //   'Upala: No such id, not an owner or not a delegate of the id'  // todo better 'No Upala ID'?
+    // )
+    // existing UpalaID and valid owner but score assigned to non-existant delegate
+
+  })
+
+
     // register UpalaID for the persona
     // try myScore on non-existent persona delegate
+    // 
     // should throw
     // register persona delegate
     // try myScore on empty pool
@@ -142,6 +168,23 @@ describe('SCORING AND BOT ATTACK', function () {
     // check reward
     // check UpalaID is deleted
     // try attack by delegate address
+})
+/*
+describe('SCORING AND BOT ATTACK', function () {
+  before('register users', async () => {
+    ;[upala, fakeDai, wallets] = await setupProtocol()
+    ;[upalaAdmin, manager1] = wallets.slice(0, 2)
+    ;[signedScoresPoolFactory, signedScoresPool] = await setUpPoolFactoryAndPool(
+      upala,
+      fakeDai,
+      'SignedScoresPoolFactory',
+      upalaAdmin,
+      manager1
+    )
+  })
+
+
+
   })
 
   it('you can explode, you can explode, you can explode, anyone can exploooooode', async function () {
