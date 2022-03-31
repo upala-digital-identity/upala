13d764d8c82dd1534844303efaf933a0e90885d9
Petr Porobov <porobov.p@gmail.com>
2022-03-31 15:14:04 +0400

    Graph demo works locally

--- scripts/graph-demo.js
+++ scripts/graph-demo.js
@@ -20,61 +20,72 @@ const USER_RATING_42 = 42 // do not use 42 anywhere else
 const BASE_SCORE = ethers.utils.parseEther('2.5')
 const RANDOM_ADDRESS = '0x0c2788f417685706f61414e4Cb6F5f672eA79731'
 
-// GROUP MANAGER ACTIONS
-let env = await setupProtocol({ isSavingConstants: false })
-let upalaAdmin, manager1, persona1, persona2, persona3, persona4, delegate11, dapp, nobody
-;[upalaAdmin, manager1, persona1, persona2, persona3, persona4, delegate11, dapp, nobody] = env.wallets
+async function main() {
+    // GROUP MANAGER ACTIONS
+    let env = await setupProtocol({ isSavingConstants: false })
+    let upalaAdmin, manager1, persona1, persona2, persona3, persona4, delegate11, dapp, nobody
+    ;[upalaAdmin, manager1, persona1, persona2, persona3, persona4, delegate11, dapp, nobody] = env.wallets
 
-let upala = env.upala
-let fakeDAI = env.dai
+    let upala = env.upala
+    let fakeDAI = env.dai
 
-// create pool
-let signedScoresPool = await deployPool('SignedScoresPool', manager1, env.upalaConstants)
-// transfer DAI to pool address
-await fakeDAI.connect(manager1).freeDaiToTheWorld(signedScoresPool.address, BASE_SCORE.mul(USER_RATING_42))
-// set base score
-let poolManager = new PoolManager(signedScoresPool, localDBdir, scoreExplorerDBdir)
-await poolManager.setBaseScore(BASE_SCORE)
+    // create pool
+    let signedScoresPool = await deployPool('SignedScoresPool', manager1, env.upalaConstants)
+    // transfer DAI to pool address
+    await fakeDAI.connect(manager1).freeDaiToTheWorld(signedScoresPool.address, BASE_SCORE.mul(USER_RATING_42))
+    
+    // INIT POOL MANAGER
+    // a hack to keep project dir clean
+    // (creates temporary folder to store bundles info)
+    let localDBdir = 'local-db-mock'
+    let scoreExplorerDBdir = 'score-exp-mock'
+    if (fs.existsSync(localDBdir)) {
+        fs.rmSync(localDBdir, { recursive: true, force: true })
+    }
+    if (fs.existsSync(scoreExplorerDBdir)) {
+        fs.rmSync(scoreExplorerDBdir, { recursive: true, force: true })
+    }
+    // todo HACK initializing 2 pool managers because of a bug (see group manager _requireCleanQueue)
+    let poolManager1 = new PoolManager(signedScoresPool, localDBdir, scoreExplorerDBdir)
+    let poolManager2 = new PoolManager(signedScoresPool, localDBdir, scoreExplorerDBdir)
+    // set base score
+    await poolManager1.setBaseScore(BASE_SCORE)
+    // publish 2 score bundles
+    // register users
+    // todo let persona2 be one of the addresses under bot-manager control
+    let users1 = [
+        { address: persona1.address, score: USER_RATING_42 },
+        { address: persona2.address, score: USER_RATING_42 + 1 },
+    ]
+    let users2 = [
+        { address: persona3.address, score: USER_RATING_42 + 2 },
+        { address: persona4.address, score: USER_RATING_42 + 3 },
+    ]
+    let subBundle1 = await poolManager1.publishNew(users1)
+    let subBundle2 = await poolManager2.publishNew(users2)
 
-// a hack to keep project dir clean
-// (creates temporary folder to store bundles info)
-let localDBdir = 'local-db-mock'
-let scoreExplorerDBdir = 'score-exp-mock'
-if (fs.existsSync(localDBdir)) {
-    fs.rmSync(localDBdir, { recursive: true, force: true })
+    // delete bundle
+    await poolManager1.deleteScoreBundleId(subBundle2.public.bundleID) 
+    // todo create another pool 
+
+    // USER ACTIONS
+    // create id
+    let persona1id = await newIdentity(persona1.address, persona1, env.upalaConstants)
+    let persona2id = await newIdentity(persona2.address, persona2, env.upalaConstants)
+
+    // // register persona1 delegate
+    await upala.connect(delegate11).approveDelegation(persona1id)
+    await upala.connect(persona1).approveDelegate(delegate11.address)
+
+    // change owner
+
+    // DAPPS ACTIONS
+    // register dapp
 }
-if (fs.existsSync(scoreExplorerDBdir)) {
-    fs.rmSync(scoreExplorerDBdir, { recursive: true, force: true })
-}
 
-// publish 2 score bundles
-await poolManager.setBaseScore(BASE_SCORE)
-// register users
-// todo let persona2 be one of the addresses under bot-manager control
-let users1 = [
-    { address: persona1.address, score: USER_RATING_42 },
-    { address: persona2.address, score: USER_RATING_42 + 1 },
-]
-let users2 = [
-    { address: persona3.address, score: USER_RATING_42 + 2 },
-    { address: persona4.address, score: USER_RATING_42 + 3 },
-]
-let subBundle1 = await poolManager.publishNew(users1)
-let subBundle2 = await poolManager.publishNew(users2)
-
-// delete bundle
-await poolManager.deleteScoreBundleId(subBundle2) 
-
-// USER ACTIONS
-// create id
-let persona1id = await newIdentity(persona1.address, persona1, env.upalaConstants)
-let persona2id = await newIdentity(persona2.address, persona2, env.upalaConstants)
-
-// register persona1 delegate
-await upala.connect(delegate11).approveDelegation(persona1id)
-await upala.connect(persona1).approveDelegate(delegate11.address)
-
-// change owner
-
-// DAPPS ACTIONS
-// register dapp
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
