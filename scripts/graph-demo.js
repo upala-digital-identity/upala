c04081fe5f3f4fc4e4826a4fd18fe8561fd20451
Petr Porobov <porobov@users.noreply.github.com>
2022-04-21 11:23:08 +0400

    Clean up in graph demo

--- scripts/graph-demo.js
+++ scripts/graph-demo.js
@@ -37,35 +37,42 @@ async function main() {
     dapp,
     nobody
 
-    // SETUP ENVIRONMENT (comment if loading from disk)
-    // let env = await setupProtocol({ isSavingConstants: false })
-    // ;[upalaAdmin, manager1, persona1, persona2, persona3, persona4, delegate11, dapp, nobody] = env.wallets
-    // let upala = env.upala
-    // let fakeDAI = env.dai
-    // let upalaConstants = env.upalaConstants
+  // BEGIN. SETUP ENVIRONMENT (comment if loading from upala constants)
+  // let env = await setupProtocol({ isSavingConstants: false })
+  // ;[upalaAdmin, manager1, persona1, persona2, persona3, persona4, delegate11, dapp, nobody] = env.wallets
+  // let upala = env.upala
+  // let fakeDAI = env.dai
+  // let upalaConstants = env.upalaConstants
+  // END
 
-    // LOAD ENVIRONMENT FROM UPALA CONSTANTS (comment if deploying anew)
+  // BEGIN. LOAD ENVIRONMENT FROM UPALA CONSTANTS (comment if deploying anew)
   ;[upalaAdmin, manager1, persona1, persona2, persona3, persona4, delegate11, dapp, nobody] = await ethers.getSigners()
   let upalaConstants = new UpalaConstants(await upalaAdmin.getChainId())
   let upala = upalaConstants.getContract('Upala', upalaAdmin)
   let fakeDAI = upalaConstants.getContract('DAI', upalaAdmin)
+  // END
 
   console.log(
-    chalk.green.bold('\nAddresses: '),
+    chalk.green.bold('\nProtocol: '),
     chalk.green('\nupalaAdmin: '),
     upalaAdmin.address,
-    chalk.green('\nmanager1: '),
-    manager1.address,
     chalk.green('\nupala:'),
-    upala.address
+    upala.address,
+    chalk.green('\nDAI:'),
+    fakeDAI.address,
   )
 
   // create pool
+  console.log(
+    chalk.green.bold('\nPool: '),
+    chalk.green('\nmanager1: '),
+    manager1.address
+  )
   let signedScoresPool = await deployPool('SignedScoresPool', manager1, upalaConstants)
   console.log(chalk.green('signedScoresPool:'), signedScoresPool.address)
   // transfer DAI to pool address
   await fakeDAI.connect(manager1).freeDaiToTheWorld(signedScoresPool.address, POOL_FUNDING)
-  console.log(chalk.green('fakeDAI sent'))
+  console.log(chalk.gray('fakeDAI sent'))
 
   // INIT POOL MANAGER
   // a hack to keep project dir clean
@@ -83,7 +90,7 @@ async function main() {
   let poolManager2 = new PoolManager(signedScoresPool, localDBdir, scoreExplorerDBdir)
   // set base score
   await poolManager1.setBaseScore(BASE_SCORE)
-  console.log(chalk.green('BASE_SCORE set'))
+  console.log(chalk.green('Base score: '), BASE_SCORE)
   // publish 2 score bundles
   // register users
   // todo let persona2 be one of the addresses under bot-manager control
@@ -111,6 +118,13 @@ async function main() {
 
   // USER ACTIONS
   // create id
+  console.log(
+    chalk.green.bold('\nUsers: '),
+    chalk.green('\npersona1: '),
+    persona1.address,
+    chalk.green('\ndelegate11: '),
+    delegate11.address,
+  )
   let persona1id = await newIdentity(persona1.address, persona1, upalaConstants)
   console.log(chalk.green('persona1id'), persona1id)
   let persona2id = await newIdentity(persona2.address, persona2, upalaConstants)
@@ -119,11 +133,14 @@ async function main() {
   // // register persona1 delegate
   tx = await upala.connect(delegate11).approveDelegation(persona1id)
   await tx.wait(numConfirmations(await delegate11.getChainId()))
-  console.log(chalk.green('delegate11 approved delegation'))
+  console.log(chalk.gray('delegate11 approved delegation'))
 
   tx = await upala.connect(persona1).approveDelegate(delegate11.address)
   await tx.wait(numConfirmations(await delegate11.getChainId()))
-  console.log(chalk.green('persona1 approved delegate11 as delegate'))
+  console.log(chalk.gray('persona1 approved delegate11 as delegate'))
+
+
+
 
   // DAPPS ACTIONS
   // register dapp
@@ -142,36 +159,31 @@ async function main() {
     subBundle2.public.signedUsers[1].signature
   )
 }
-/* output 06.04.2022 (constants not saved)
-Upala constants: networkID undefined
-Upala constants: networkID undefined
-Upala constants: networkID undefined
-Upala constants: networkID undefined
-
-Addresses:  
+/* output 20.04.2022 (constants not saved)
+Protocol:  
 upalaAdmin:  0xa88630300706488e9d31597ccC4394206F4D4C6C 
+upala: 0x59FcEd8395A4d97F0b9F9e1Fd43C4B0C9ff71e53 
+DAI: 0xbC0dFaA78fe7bc8248b9F857292f680a1630b0C5
+
+Pool:  
 manager1:  0xb94f953f389c45AD3fb71dC917f09eE9DF89e722
-Upala constants: networkID 4
-signedScoresPool: 0xa513E588238b69f4A87c79ebD4EB41CDE00EFC45
+signedScoresPool: 0xe9b4513cec3FA62027A922fA4b56E3023BE89980
 fakeDAI sent
-BASE_SCORE set
-Upala constants: networkID 4
+Base score:  BigNumber { _hex: '0x1bc16d674ec80000', _isBigNumber: true }
 subBundle1: 0x00000000000000000000000000000000598de248af93c7b853abcc219893cd07
-Upala constants: networkID 4
 subBundle2: 0x00000000000000000000000000000000389f83dcc81ef8f024ab3b9814105e2f
-Upala constants: networkID 4
 subBundle1 deleted
-Upala constants: networkID 4
-persona1id 0xCeaC9a972bD28cf8f8100ab861C6b9fBD7b8140D
-Upala constants: networkID 4
-persona2id 0xE135a1338BB4dc3fcB7Bc1D98712B81649C24e24
-Upala constants: networkID 4
+
+Users:  
+persona1:  0xd6287A66771aD5Ff1E56a4aA21F5B424b90A7fAF 
+delegate11:  0x613bb366C1C14E506e077fF71B8537B9877F8c76
+persona1id 0x242D51Ff0d2190A71A3f9C25D299F99b0f17f926
+persona2id 0x8F799CF0Adec53947813d3fd3E3D90056Faf2b67
 delegate11 approved delegation
-Upala constants: networkID 4
 persona1 approved delegate11 as delegate
 
 Bot attack payload example:  
-poolAddress:  0xa513E588238b69f4A87c79ebD4EB41CDE00EFC45 
+poolAddress:  0xe9b4513cec3FA62027A922fA4b56E3023BE89980 
 scoreAssignedTo:  0x1633092577b6e789863E8284d3db1393259e5D08 
 score:  50 
 bundleId:  0x00000000000000000000000000000000389f83dcc81ef8f024ab3b9814105e2f 
